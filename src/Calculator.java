8221ba78e9f1986c789556d088a6b63b5e9802ae
Frank <fha@easv.dk>
2023-12-21 15:02:07 +0100

    start

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public int addTwoDigits(int addends1, int addends2)
+    {
+        return addends1+addends2;
+    }
+
+    public int SubtractTwoDigits(int minuend , int subtrahend )
+    {
+        return minuend - subtrahend;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+
+        System.out.printf("3+5 = %d" , calc.addTwoDigits(3,5));
+    }
+}
